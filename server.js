e58c50c9c7758712fbc22c8f0b82915deb80eb76
Glitch (gregarious-puddle-water) <none>
2020-08-07 01:17:15 +0000

    👕🔮 Updated with Glitch

--- server.js
+++ server.js
@@ -24,9 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/:date_string?', function(req, res) {
+  var strDate = req.params.date_string;
+  var date = new Date(strDate);  
+  
+  if(strDate != undefined && strDate.indexOf("-") == -1) { // if not a string date
+    date = new Date(parseInt(strDate));
+  }  else if (!strDate) {                                  // if there is nothing written
+    date = new Date();
+  }
+  
+  if(date != "Invalid Date") {                             // if it's a valid date
+    res.json({"unix": date.getTime(), "utc": date.toUTCString()})
+  } else if(strDate != undefined && strDate.length > 0) {  // if there is something but it's not a date
+    res.json({"error" : "Invalid Date" })
+  } 
+});
+
+
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+console.log("Hello");
